11b93ff87791613427730c54a2f45b6c8c998408
shoui520 <shouinekomimi@gmail.com>
2026-07-06 21:04:31 +0100

    ipu: convert yuv blocks through cortex-a9 neon

--- pcsx2/IPU/yuv2rgb.cpp
+++ pcsx2/IPU/yuv2rgb.cpp
@@ -6,6 +6,10 @@
 #include "IPU/IPU_MultiISA.h"
 #include "IPU/yuv2rgb.h"
 
+#if defined(VITASX2_QEMU_VALIDATION)
+u32 g_qemuIpuYuv2RgbNeonBlocks = 0;
+#endif
+
 // The IPU's colour space conversion conforms to ITU-R Recommendation BT.601 if anyone wants to make a
 // faster or "more accurate" implementation, but this is the precise documented integer method used by
 // the hardware and is fast enough with SSE2.
@@ -136,82 +140,89 @@ __ri void yuv2rgb_sse2()
 	}
 }
 
-#elif defined(ARCH_ARM64)
+#elif defined(ARCH_ARM64) || defined(ARCH_ARM32)
 
-#if defined(_MSC_VER) && !defined(__clang__)
+#if defined(ARCH_ARM64) && defined(_MSC_VER) && !defined(__clang__)
 #include <arm64_neon.h>
 #else
 #include <arm_neon.h>
 #endif
 
-#define MULHI16(a, b) vshrq_n_s16(vqdmulhq_s16((a), (b)), 1)
+static __forceinline int8x16_t LoadChromaSignedShifted(const u8* chroma)
+{
+	const int8x8_t c_bias = vdup_n_s8(s8(IPU_C_BIAS));
+	int8x8_t bytes = vld1_s8(reinterpret_cast<const s8*>(chroma));
+	bytes = veor_s8(bytes, c_bias);
+	const int8x8x2_t shifted = vzip_s8(vdup_n_s8(0), bytes);
+	return vcombine_s8(shifted.val[0], shifted.val[1]);
+}
+
+static __forceinline int16x8_t MulHiS16(int16x8_t a, int16x8_t b)
+{
+	return vshrq_n_s16(vqdmulhq_s16(a, b), 1);
+}
+
+static __forceinline int16x8_t MulHiU16(uint16x8_t a, uint16x8_t b)
+{
+	const uint32x4_t lo = vmull_u16(vget_low_u16(a), vget_low_u16(b));
+	const uint32x4_t hi = vmull_u16(vget_high_u16(a), vget_high_u16(b));
+	return vreinterpretq_s16_u16(vcombine_u16(vshrn_n_u32(lo, 16), vshrn_n_u32(hi, 16)));
+}
+
+static __forceinline uint8x16_t InterleaveEvenOdd(uint8x8_t even, uint8x8_t odd)
+{
+	const uint8x8x2_t zipped = vzip_u8(even, odd);
+	return vcombine_u8(zipped.val[0], zipped.val[1]);
+}
 
 __ri void yuv2rgb_neon()
 {
-	const int8x16_t c_bias = vdupq_n_s8(s8(IPU_C_BIAS));
 	const uint8x16_t y_bias = vdupq_n_u8(IPU_Y_BIAS);
-	const int16x8_t y_mask = vdupq_n_s16(s16(0xFF00));
+	const uint16x8_t y_mask = vdupq_n_u16(0xFF00);
 	// Specifying round off instead of round down as everywhere else
 	// implies that this is right
 	const int16x8_t round_1bit = vdupq_n_s16(0x0001);
-	;
 
-	const int16x8_t y_coefficient = vdupq_n_s16(s16(IPU_Y_COEFF << 2));
+	const uint16x8_t y_coefficient = vdupq_n_u16(IPU_Y_COEFF << 2);
 	const int16x8_t gcr_coefficient = vdupq_n_s16(s16(u16(IPU_GCR_COEFF) << 2));
 	const int16x8_t gcb_coefficient = vdupq_n_s16(s16(u16(IPU_GCB_COEFF) << 2));
 	const int16x8_t rcr_coefficient = vdupq_n_s16(s16(IPU_RCR_COEFF << 2));
 	const int16x8_t bcb_coefficient = vdupq_n_s16(s16(IPU_BCB_COEFF << 2));
 
 	// Alpha set to 0x80 here. The threshold stuff is done later.
-	const uint8x16_t alpha = vreinterpretq_u8_s8(c_bias);
+	const uint8x16_t alpha = vdupq_n_u8(IPU_C_BIAS);
 
 	for (int n = 0; n < 8; ++n)
 	{
 		// could skip the loadl_epi64 but most SSE instructions require 128-bit
 		// alignment so two versions would be needed.
-		int8x16_t cb = vcombine_s8(vld1_s8(reinterpret_cast<s8*>(&decoder.mb8.Cb[n][0])), vdup_n_s8(0));
-		int8x16_t cr = vcombine_s8(vld1_s8(reinterpret_cast<s8*>(&decoder.mb8.Cr[n][0])), vdup_n_s8(0));
+		const int8x16_t cb = LoadChromaSignedShifted(&decoder.mb8.Cb[n][0]);
+		const int8x16_t cr = LoadChromaSignedShifted(&decoder.mb8.Cr[n][0]);
 
-		// (Cb - 128) << 8, (Cr - 128) << 8
-		cb = veorq_s8(cb, c_bias);
-		cr = veorq_s8(cr, c_bias);
-		cb = vzip1q_s8(vdupq_n_s8(0), cb);
-		cr = vzip1q_s8(vdupq_n_s8(0), cr);
-
-		int16x8_t rc = MULHI16(vreinterpretq_s16_s8(cr), rcr_coefficient);
-		int16x8_t gc = vqaddq_s16(MULHI16(vreinterpretq_s16_s8(cr), gcr_coefficient), MULHI16(vreinterpretq_s16_s8(cb), gcb_coefficient));
-		int16x8_t bc = MULHI16(vreinterpretq_s16_s8(cb), bcb_coefficient);
+		const int16x8_t rc = MulHiS16(vreinterpretq_s16_s8(cr), rcr_coefficient);
+		const int16x8_t gc = vqaddq_s16(MulHiS16(vreinterpretq_s16_s8(cr), gcr_coefficient), MulHiS16(vreinterpretq_s16_s8(cb), gcb_coefficient));
+		const int16x8_t bc = MulHiS16(vreinterpretq_s16_s8(cb), bcb_coefficient);
 
 		for (int m = 0; m < 2; ++m)
 		{
 			uint8x16_t y = vld1q_u8(&decoder.mb8.Y[n * 2 + m][0]);
 			y = vqsubq_u8(y, y_bias);
 			// Y << 8 for pixels 0, 2, 4, 6, 8, 10, 12, 14
-			int16x8_t y_even = vshlq_n_s16(vreinterpretq_s16_u8(y), 8);
+			uint16x8_t y_even = vshlq_n_u16(vreinterpretq_u16_u8(y), 8);
 			// Y << 8 for pixels 1, 3, 5, 7 ,9, 11, 13, 15
-			int16x8_t y_odd = vandq_s16(vreinterpretq_s16_u8(y), y_mask);
+			uint16x8_t y_odd = vandq_u16(vreinterpretq_u16_u8(y), y_mask);
 
 			// y_even = _mm_mulhi_epu16(y_even, y_coefficient);
 			// y_odd = _mm_mulhi_epu16(y_odd, y_coefficient);
+			const int16x8_t y_even_mul = MulHiU16(y_even, y_coefficient);
+			const int16x8_t y_odd_mul = MulHiU16(y_odd, y_coefficient);
 
-			uint16x4_t a3210 = vget_low_u16(vreinterpretq_u16_s16(y_even));
-			uint16x4_t b3210 = vget_low_u16(vreinterpretq_u16_s16(y_coefficient));
-			uint32x4_t ab3210 = vmull_u16(a3210, b3210);
-			uint32x4_t ab7654 = vmull_high_u16(vreinterpretq_u16_s16(y_even), vreinterpretq_u16_s16(y_coefficient));
-			y_even = vreinterpretq_s16_u16(vuzp2q_u16(vreinterpretq_u16_u32(ab3210), vreinterpretq_u16_u32(ab7654)));
-
-			a3210 = vget_low_u16(vreinterpretq_u16_s16(y_odd));
-			b3210 = vget_low_u16(vreinterpretq_u16_s16(y_coefficient));
-			ab3210 = vmull_u16(a3210, b3210);
-			ab7654 = vmull_high_u16(vreinterpretq_u16_s16(y_odd), vreinterpretq_u16_s16(y_coefficient));
-			y_odd = vreinterpretq_s16_u16(vuzp2q_u16(vreinterpretq_u16_u32(ab3210), vreinterpretq_u16_u32(ab7654)));
-
-			int16x8_t r_even = vqaddq_s16(rc, y_even);
-			int16x8_t r_odd = vqaddq_s16(rc, y_odd);
-			int16x8_t g_even = vqaddq_s16(gc, y_even);
-			int16x8_t g_odd = vqaddq_s16(gc, y_odd);
-			int16x8_t b_even = vqaddq_s16(bc, y_even);
-			int16x8_t b_odd = vqaddq_s16(bc, y_odd);
+			int16x8_t r_even = vqaddq_s16(rc, y_even_mul);
+			int16x8_t r_odd = vqaddq_s16(rc, y_odd_mul);
+			int16x8_t g_even = vqaddq_s16(gc, y_even_mul);
+			int16x8_t g_odd = vqaddq_s16(gc, y_odd_mul);
+			int16x8_t b_even = vqaddq_s16(bc, y_even_mul);
+			int16x8_t b_odd = vqaddq_s16(bc, y_odd_mul);
 
 			// round
 			r_even = vshrq_n_s16(vaddq_s16(r_even, round_1bit), 1);
@@ -222,35 +233,30 @@ __ri void yuv2rgb_neon()
 			b_odd = vshrq_n_s16(vaddq_s16(b_odd, round_1bit), 1);
 
 			// combine even and odd bytes in original order
-			uint8x16_t r = vcombine_u8(vqmovun_s16(r_even), vqmovun_s16(r_odd));
-			uint8x16_t g = vcombine_u8(vqmovun_s16(g_even), vqmovun_s16(g_odd));
-			uint8x16_t b = vcombine_u8(vqmovun_s16(b_even), vqmovun_s16(b_odd));
-
-			r = vzip1q_u8(r, vreinterpretq_u8_u64(vdupq_laneq_u64(vreinterpretq_u64_u8(r), 1)));
-			g = vzip1q_u8(g, vreinterpretq_u8_u64(vdupq_laneq_u64(vreinterpretq_u64_u8(g), 1)));
-			b = vzip1q_u8(b, vreinterpretq_u8_u64(vdupq_laneq_u64(vreinterpretq_u64_u8(b), 1)));
-
-			// Create RGBA (we could generate A here, but we don't) quads
-			uint8x16_t rg_l = vzip1q_u8(r, g);
-			uint8x16_t ba_l = vzip1q_u8(b, alpha);
-			uint16x8_t rgba_ll = vzip1q_u16(vreinterpretq_u16_u8(rg_l), vreinterpretq_u16_u8(ba_l));
-			uint16x8_t rgba_lh = vzip2q_u16(vreinterpretq_u16_u8(rg_l), vreinterpretq_u16_u8(ba_l));
-
-			uint8x16_t rg_h = vzip2q_u8(r, g);
-			uint8x16_t ba_h = vzip2q_u8(b, alpha);
-			uint16x8_t rgba_hl = vzip1q_u16(vreinterpretq_u16_u8(rg_h), vreinterpretq_u16_u8(ba_h));
-			uint16x8_t rgba_hh = vzip2q_u16(vreinterpretq_u16_u8(rg_h), vreinterpretq_u16_u8(ba_h));
-
-			vst1q_u8(reinterpret_cast<u8*>(&decoder.rgb32.c[n * 2 + m][0]), vreinterpretq_u8_u16(rgba_ll));
-			vst1q_u8(reinterpret_cast<u8*>(&decoder.rgb32.c[n * 2 + m][4]), vreinterpretq_u8_u16(rgba_lh));
-			vst1q_u8(reinterpret_cast<u8*>(&decoder.rgb32.c[n * 2 + m][8]), vreinterpretq_u8_u16(rgba_hl));
-			vst1q_u8(reinterpret_cast<u8*>(&decoder.rgb32.c[n * 2 + m][12]), vreinterpretq_u8_u16(rgba_hh));
+			const uint8x16_t r = InterleaveEvenOdd(vqmovun_s16(r_even), vqmovun_s16(r_odd));
+			const uint8x16_t g = InterleaveEvenOdd(vqmovun_s16(g_even), vqmovun_s16(g_odd));
+			const uint8x16_t b = InterleaveEvenOdd(vqmovun_s16(b_even), vqmovun_s16(b_odd));
+			const uint8x16x4_t rgba = {r, g, b, alpha};
+			vst4q_u8(reinterpret_cast<u8*>(&decoder.rgb32.c[n * 2 + m][0]), rgba);
 		}
 	}
+
+#if defined(VITASX2_QEMU_VALIDATION)
+	++::g_qemuIpuYuv2RgbNeonBlocks;
+#endif
+}
+#endif
+
+#if defined(VITASX2_QEMU_VALIDATION)
+void IpuYuv2RgbReferenceForValidation()
+{
+	yuv2rgb_reference();
 }
 
-#undef MULHI16
-
+void IpuYuv2RgbSelectedForValidation()
+{
+	yuv2rgb();
+}
 #endif
 
 MULTI_ISA_UNSHARED_END
--- pcsx2/IPU/yuv2rgb.h
+++ pcsx2/IPU/yuv2rgb.h
@@ -12,7 +12,7 @@ MULTI_ISA_DEF(extern void yuv2rgb_reference();)
 #define yuv2rgb yuv2rgb_sse2
 MULTI_ISA_DEF(extern void yuv2rgb_sse2();)
 
-#elif defined(ARCH_ARM64)
+#elif defined(ARCH_ARM64) || defined(ARCH_ARM32)
 
 #define yuv2rgb yuv2rgb_neon
 MULTI_ISA_DEF(extern void yuv2rgb_neon();)
