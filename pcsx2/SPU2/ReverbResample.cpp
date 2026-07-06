4b024aeb6e87b3049b9be0df7982e15e446250e8
shoui520 <shouinekomimi@gmail.com>
2026-07-06 18:28:25 +0100

    spu2: add arm32 neon reverb resample

--- pcsx2/SPU2/ReverbResample.cpp
+++ pcsx2/SPU2/ReverbResample.cpp
@@ -4,6 +4,15 @@
 #include "GS/GSVector.h"
 #include "SPU2/defs.h"
 
+#if defined(ARCH_ARM32)
+#include <arm_neon.h>
+#endif
+
+#if defined(VITASX2_QEMU_VALIDATION)
+u32 g_qemuSpu2ReverbDownsampleNeon = 0;
+u32 g_qemuSpu2ReverbUpsampleNeon = 0;
+#endif
+
 MULTI_ISA_UNSHARED_START
 
 static constexpr u32 NUM_TAPS = 39;
@@ -79,6 +88,42 @@ s32 __forceinline ReverbDownsample_reference(V_Core& core, bool right)
 	return clamp_mix(out);
 }
 
+#if defined(ARCH_ARM32)
+static __forceinline int32x4_t ReverbAccumulate8(int32x4_t acc, const s16* samples, const s16* coefs)
+{
+	const int16x8_t sample_vec = vld1q_s16(samples);
+	const int16x8_t coef_vec = vld1q_s16(coefs);
+	acc = vmlal_s16(acc, vget_low_s16(sample_vec), vget_low_s16(coef_vec));
+	acc = vmlal_s16(acc, vget_high_s16(sample_vec), vget_high_s16(coef_vec));
+	return acc;
+}
+
+static __forceinline s32 ReverbFinishAccumulation(int32x4_t acc)
+{
+	int32x2_t sum = vadd_s32(vget_low_s32(acc), vget_high_s32(acc));
+	sum = vpadd_s32(sum, sum);
+	return clamp_mix(vget_lane_s32(sum, 0) >> 15);
+}
+
+s32 __forceinline ReverbDownsample_neon(V_Core& core, bool right)
+{
+	const int index = (core.RevbSampleBufPos - NUM_TAPS) & 63;
+	const s16* samples = &core.RevbDownBuf[right][index];
+	int32x4_t acc = vdupq_n_s32(0);
+
+	acc = ReverbAccumulate8(acc, &samples[0], &filter_down_coefs[0]);
+	acc = ReverbAccumulate8(acc, &samples[8], &filter_down_coefs[8]);
+	acc = ReverbAccumulate8(acc, &samples[16], &filter_down_coefs[16]);
+	acc = ReverbAccumulate8(acc, &samples[24], &filter_down_coefs[24]);
+	acc = ReverbAccumulate8(acc, &samples[32], &filter_down_coefs[32]);
+
+#if defined(VITASX2_QEMU_VALIDATION)
+	++::g_qemuSpu2ReverbDownsampleNeon;
+#endif
+	return ReverbFinishAccumulation(acc);
+}
+#endif
+
 #if !defined(ARCH_ARM32)
 #if _M_SSE >= 0x501
 s32 __forceinline ReverbDownsample_avx(V_Core& core, bool right)
@@ -142,7 +187,7 @@ s32 __forceinline ReverbDownsample_sse(V_Core& core, bool right)
 s32 ReverbDownsample(V_Core& core, bool right)
 {
 #if defined(ARCH_ARM32)
-	return ReverbDownsample_reference(core, right);
+	return ReverbDownsample_neon(core, right);
 #elif _M_SSE >= 0x501
 	return ReverbDownsample_avx(core, right);
 #else
@@ -166,6 +211,33 @@ StereoOut32 __forceinline ReverbUpsample_reference(V_Core& core)
 
 	return {clamp_mix(l), clamp_mix(r)};
 }
+
+#if defined(ARCH_ARM32)
+StereoOut32 __forceinline ReverbUpsample_neon(V_Core& core)
+{
+	const int index = (core.RevbSampleBufPos - NUM_TAPS) & 63;
+	const s16* left = &core.RevbUpBuf[0][index];
+	const s16* right = &core.RevbUpBuf[1][index];
+	int32x4_t left_acc = vdupq_n_s32(0);
+	int32x4_t right_acc = vdupq_n_s32(0);
+
+	left_acc = ReverbAccumulate8(left_acc, &left[0], &filter_up_coefs[0]);
+	right_acc = ReverbAccumulate8(right_acc, &right[0], &filter_up_coefs[0]);
+	left_acc = ReverbAccumulate8(left_acc, &left[8], &filter_up_coefs[8]);
+	right_acc = ReverbAccumulate8(right_acc, &right[8], &filter_up_coefs[8]);
+	left_acc = ReverbAccumulate8(left_acc, &left[16], &filter_up_coefs[16]);
+	right_acc = ReverbAccumulate8(right_acc, &right[16], &filter_up_coefs[16]);
+	left_acc = ReverbAccumulate8(left_acc, &left[24], &filter_up_coefs[24]);
+	right_acc = ReverbAccumulate8(right_acc, &right[24], &filter_up_coefs[24]);
+	left_acc = ReverbAccumulate8(left_acc, &left[32], &filter_up_coefs[32]);
+	right_acc = ReverbAccumulate8(right_acc, &right[32], &filter_up_coefs[32]);
+
+#if defined(VITASX2_QEMU_VALIDATION)
+	++::g_qemuSpu2ReverbUpsampleNeon;
+#endif
+	return {ReverbFinishAccumulation(left_acc), ReverbFinishAccumulation(right_acc)};
+}
+#endif
 
 #if !defined(ARCH_ARM32)
 #if _M_SSE >= 0x501
@@ -257,12 +329,24 @@ StereoOut32 __forceinline ReverbUpsample_sse(V_Core& core)
 StereoOut32 ReverbUpsample(V_Core& core)
 {
 #if defined(ARCH_ARM32)
-	return ReverbUpsample_reference(core);
+	return ReverbUpsample_neon(core);
 #elif _M_SSE >= 0x501
 	return ReverbUpsample_avx(core);
 #else
 	return ReverbUpsample_sse(core);
 #endif
 }
+
+#if defined(VITASX2_QEMU_VALIDATION)
+s32 ReverbDownsampleScalarForValidation(V_Core& core, bool right)
+{
+	return ReverbDownsample_reference(core, right);
+}
+
+StereoOut32 ReverbUpsampleScalarForValidation(V_Core& core)
+{
+	return ReverbUpsample_reference(core);
+}
+#endif
 
 MULTI_ISA_UNSHARED_END
