65d9d8a7f62ff29fc340560e65261dae118de1f3
shoui520 <shouinekomimi@gmail.com>
2026-07-07 02:32:56 +0100

    spu2: clamp adpcm decode with ssat

--- pcsx2/SPU2/Mixer.cpp
+++ pcsx2/SPU2/Mixer.cpp
@@ -42,6 +42,7 @@ u32 g_qemuSpu2MasterVolumeSlideSkipped = 0;
 u32 g_qemuSpu2ZeroVoiceGateSkipped = 0;
 u32 g_qemuSpu2NonzeroVoiceGateMixed = 0;
 u32 g_qemuSpu2VoiceInterpNeon = 0;
+u32 g_qemuSpu2AdpcmSsatClamps = 0;
 #endif
 
 MULTI_ISA_UNSHARED_START
@@ -54,7 +55,27 @@ static const s32 tbl_XA_Factor[16][2] =
 		{98, -55},
 		{122, -60}};
 
-static void __forceinline XA_decode_block(s16* buffer, const s16* block, s32& prev1, s32& prev2)
+static __forceinline s32 ClampAdpcmSample_reference(s32 value)
+{
+	return std::clamp<s32>(value, -0x8000, 0x7fff);
+}
+
+static __forceinline s32 ClampAdpcmSample_selected(s32 value)
+{
+#if defined(ARCH_ARM32)
+	s32 result;
+	__asm__("ssat %0, #16, %1" : "=r"(result) : "r"(value));
+#if defined(VITASX2_QEMU_VALIDATION)
+	++::g_qemuSpu2AdpcmSsatClamps;
+#endif
+	return result;
+#else
+	return ClampAdpcmSample_reference(value);
+#endif
+}
+
+template <typename ClampSample>
+static void __forceinline XA_decode_block_impl(s16* buffer, const s16* block, s32& prev1, s32& prev2, ClampSample clamp_sample)
 {
 	const s32 header = *block;
 	const s32 shift = (header & 0xF) + 16;
@@ -72,19 +93,41 @@ static void __forceinline XA_decode_block(s16* buffer, const s16* block, s32& pr
 		s32 data = ((*blockbytes) << 28) & 0xF0000000;
 		s32 pcm = (data >> shift) + (((pred1 * prev1) + (pred2 * prev2) + 32) >> 6);
 
-		pcm = std::clamp<s32>(pcm, -0x8000, 0x7fff);
+		pcm = clamp_sample(pcm);
 		*(buffer++) = pcm;
 
 		data = ((*blockbytes) << 24) & 0xF0000000;
 		s32 pcm2 = (data >> shift) + (((pred1 * pcm) + (pred2 * prev1) + 32) >> 6);
 
-		pcm2 = std::clamp<s32>(pcm2, -0x8000, 0x7fff);
+		pcm2 = clamp_sample(pcm2);
 		*(buffer++) = pcm2;
 
 		prev2 = pcm;
 		prev1 = pcm2;
 	}
 }
+
+static void __forceinline XA_decode_block_reference(s16* buffer, const s16* block, s32& prev1, s32& prev2)
+{
+	XA_decode_block_impl(buffer, block, prev1, prev2, ClampAdpcmSample_reference);
+}
+
+static void __forceinline XA_decode_block(s16* buffer, const s16* block, s32& prev1, s32& prev2)
+{
+	XA_decode_block_impl(buffer, block, prev1, prev2, ClampAdpcmSample_selected);
+}
+
+#if defined(VITASX2_QEMU_VALIDATION)
+void Spu2XaDecodeBlockReferenceForValidation(s16* buffer, const s16* block, s32* prev1, s32* prev2)
+{
+	XA_decode_block_reference(buffer, block, *prev1, *prev2);
+}
+
+void Spu2XaDecodeBlockSelectedForValidation(s16* buffer, const s16* block, s32* prev1, s32* prev2)
+{
+	XA_decode_block(buffer, block, *prev1, *prev2);
+}
+#endif
 
 static void __forceinline IncrementNextA(V_Core& thiscore, uint voiceidx)
 {
